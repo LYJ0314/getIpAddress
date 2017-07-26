8709722cf8012788a1d412816dc77486391194ff
lyj <lyj@lyjdeimac.local>
2017-07-26 14:47:12 +0800

    Initial Commit

---
+++ GetIp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GetIp
+//
+//  Created by lyj on 17/7/26.
+//  Copyright © 2017年 lyj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GetIp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GetIp
+//
+//  Created by lyj on 17/7/26.
+//  Copyright © 2017年 lyj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
